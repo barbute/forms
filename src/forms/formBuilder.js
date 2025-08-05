61e6c84e9ba37ce61fef7b4b4f3b42e2e8c911a4
barbute <jackhdemeterio@gmail.com>
2025-08-05 14:02:55 -0500

    Read mock data

--- src/forms/formBuilder.js
+++ src/forms/formBuilder.js
@@ -1,1 +1,21 @@
+const testDataURL = "http://127.0.0.1:5500/src/data/mockdata.json";
+
 const container = document.querySelector(".container")
+let form;
+
+async function readJSON(url) {
+  const requestURL = url;
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const data = await response.json();
+
+  if (form === undefined) {
+    form = data;
+  }
+  console.log(form);
+
+  return data;
+}
+
+readJSON(testDataURL);
