061ba6795e93e76f8bdc5a6e8a0fd91e73b048be
barbute <jackhdemeterio@gmail.com>
2025-08-06 20:05:52 -0500

    Refactored date display and storing system
    All dates will be stored separately from start and end times; everything will be in UTC then converted to the users local time for displaying

--- src/forms/formBuilder.js
+++ src/forms/formBuilder.js
@@ -25,20 +25,93 @@ readJSON(testDataURL);
 const formBody = document.querySelector(".form-table-body");
 const formTitle = document.querySelector(".form-title");
 
-/*
- * TODO Should store date & time as UTC, then convert to CST/local time when
- * displaying. When inputting, use local browser time then convert to UTC
- */
-function createDateCell(date, startTime, endTime, rowspan) {
+// Stole this from GPT lol
+function to12HourTime(time24Hour) {
+  const [hourStr, minuteStr] = time24Hour.split(":");
+  let hour = parseInt(hourStr, 10);
+  const minute = minuteStr.padStart(2, "0");
+
+  const period = hour >= 12 ? "PM" : "AM";
+  hour = hour % 12;
+  if (hour === 0) {
+    hour = 12;
+  }
+
+  return `${hour}:${minute} ${period}`;
+}
+
+function toFormattedTimeLocal(dateUTC, timeUTC) {
+  const fullDateUTC = new Date(`${dateUTC}T${timeUTC}:00Z`);
+  // Date object already converts UTC to local
+  const hoursLocal = fullDateUTC.getHours();
+  const minutesLocal = fullDateUTC.getMinutes();
+  // Display as 12-hour time
+  let timeLocal;
+  if (minutesLocal.toString().length === 1) {
+    timeLocal = to12HourTime(`${hoursLocal}:0${minutesLocal}`);
+  } else {
+    timeLocal = to12HourTime(`${hoursLocal}:${minutesLocal}`);
+  }
+  return timeLocal;
+}
+
+// NOTE All times and dates are stored as UTC, thus will be converted to the
+// user's local time and date in this function
+function createDateCell(dateUTC, startTimeUTC, endTimeUTC, rowspan) {
   const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", 
     "Sept", "Oct", "Nov", "Dec"];
-  
-  console.log(date);
-  console.log(date+"T"+startTime+":00-5:00");
-  let dateCell;
-  const startDate = new Date(date+"T"+startTime+":00-5:00");
-  const endDate = new Date(date+"T"+endTime+":00-5:00")
-  dateCell = `<td rowspan=${rowspan}>${startDate.toDateString()}</td>`;
+
+  const startTimeLocal = toFormattedTimeLocal(dateUTC, startTimeUTC);
+  const endTimeLocal = toFormattedTimeLocal(dateUTC, endTimeUTC);
+
+  const monthLocal = months[new Date(dateUTC).getMonth()]; // Month lookup
+  const dayLocal = new Date(dateUTC).getDate();
+
+  let dateCell = `
+  <td rowspan=${rowspan}>
+    <p>${monthLocal} ${dayLocal}</p>
+    <p>${startTimeLocal} → ${endTimeLocal}</p>
+  </td>
+  `;
+
+  // // Get user timezone offset for converting date to local time when displaying
+  // // since times are stored in UTC
+  // let userTimezoneOffsetHours = new Date().getTimezoneOffset()/60;
+  // // Check if timezone offset is a single digit since that effects whether or
+  // // not you have to put a leading zero :/
+  // if (Math.abs(userTimezoneOffsetHours).toString().length === 1) {
+  //   // Converts to string so it can be passed into the date object with the
+  //   // other date strings
+  //   userTimezoneOffsetHours = "0" + userTimezoneOffsetHours.toString();
+  // }
+
+  // console.log("Timezone offset: " + userTimezoneOffsetHours);
+  // console.log("Test time: " + date + "T" + startTime+ ":00-" + userTimezoneOffsetHours + ":00");
+
+  // let dateCell;
+  // // const startDate = new Date(date + "T" + startTime + ":00-05:00");
+  // let offset = new Date().getTimezoneOffset()/60;
+  // let timezone = "";
+  // if (offset > 0) {
+    
+  // }
+  // console.log(date + "T" + startTime + ":00"+offset.toString());
+  // const startDate = new Date(date + "T" + startTime + ":00"+offset.toString());
+  // console.log(startDate);
+
+  // const endDate = new Date(date + "T" + endTime + ":00-05:00")
+  // dateCell = `<td rowspan=${rowspan}>${startDate.toDateString()}</td>`;
+
+  // // Date(year, month, day, hour, min, sec, ms)
+  // const d = new Date("2025-08-01T06:30:00-05:00");
+  // console.log(d);
+  // console.log(d.getFullYear());
+  // console.log(months[d.getMonth()]);
+  // console.log(d.getDate());
+  // console.log(d.getHours());
+  // console.log(d.getMinutes());
+  // console.log(d.getTimezoneOffset()/60);
+
 
   // TODO Ensure timezone conversions are correct
   // TODO Fix date display to only show month and day when it's the current year
@@ -104,7 +177,7 @@ function createCapacityCell(capacity, signUps) {
   return capacityCell;
 }
 
-function addRow(slot, description, date, startTime, endTime, signUps, 
+function addRow(slot, description, dateUTC, startTimeUTC, endTimeUTC, signUps, 
   capacity, slotsNum, currentSlot) {
   const slotCell = `
     <td>${slot}</td>
@@ -122,7 +195,7 @@ function addRow(slot, description, date, startTime, endTime, signUps,
   let row;
   // If we are on the first slot for this date, add the date cell
   if (currentSlot === 0) {
-    const dateCell = createDateCell(date, startTime, endTime, slotsNum);
+    const dateCell = createDateCell(dateUTC, startTimeUTC, endTimeUTC, slotsNum);
 
     row = `<tr>${dateCell}${slotCell}${descriptionCell}${signUpsCell}${capacityCell}</tr>`
     formBody.innerHTML += row;
@@ -154,17 +227,17 @@ function buildTable(data) {
       // Set variables that will be passed into row adder
       const slotName = entrySlot.name;
       const description = entrySlot.description;
-      const date = entryDate.date;
-      const startTime = entryDate.startTime;
-      const endTime = entryDate.endTime;
+      const dateUTC = entryDate.dateUTC;
+      const startTimeUTC = entryDate.startTimeUTC;
+      const endTimeUTC = entryDate.endTimeUTC;
       let signUps = [];
       for (let person of entry.signUps) {
         signUps.push(person.name);
       }
       const capacity = entrySlot.capacity;
 
-      addRow(slotName, 
-        description, date, startTime, endTime, signUps, capacity, entryDate.entries.length, i);
+      addRow(slotName, description, dateUTC, startTimeUTC, endTimeUTC, 
+        signUps, capacity, entryDate.entries.length, i);
     }
   }
 }
